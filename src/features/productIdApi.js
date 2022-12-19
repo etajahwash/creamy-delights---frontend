c6610bb2811fae588127d93e66f311e136397323
etajahwash <115843011+etajahwash@users.noreply.github.com>
2022-12-19 01:35:11 -0500

    Update productIdApi.js

--- src/features/productIdApi.js
+++ src/features/productIdApi.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
 export const productIdApi = createApi({
     reducerPath: 'productIdApi',
-    baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:5000/api" }),
+    baseQuery: fetchBaseQuery({ baseUrl: process.env.API_URL }),
     endpoints: (builder) => ({
         getProductById: builder.query({
             query: (id) => `products/${id}`,
