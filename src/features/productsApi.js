493eedbb3472e52e5fd2ab228de9133845dc393b
etajahwash <115843011+etajahwash@users.noreply.github.com>
2022-12-19 02:09:14 -0500

    Update productsApi.js

--- src/features/productsApi.js
+++ src/features/productsApi.js
@@ -1,8 +1,9 @@
 import { createApi , fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+import { url } from './api';
 
 export const productsApi = createApi({
     reducerPath: "productsApi",
-    baseQuery: fetchBaseQuery({ baseUrl: process.env.API_URL}),
+    baseQuery: fetchBaseQuery({ baseUrl: url}),
     endpoints: (builder) => ({
         getAllProducts: builder.query({
             query: () => "products",
